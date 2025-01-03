d1af87bcd6ccb776fff3513876ca47f0bd004a79
Koderua <kibirtbayou@yahoo.com>
2025-01-03 14:18:40 +0300

    use json server to fetch api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor ullam provident a, quis eligendi quo quos cupiditate sequi inventore eos corrupti maiores explicabo accusantium, aliquam pariatur maxime unde, libero ipsa dignissimos quam? Numquam quibusdam non et praesentium, hic possimus quisquam velit consectetur vel at quasi cupiditate repellat ipsam id! Odit iure quo dolore natus recusandae soluta, autem, quidem quia quod aut suscipit accusantium iusto
+      at qui quam architecto quisquam officiis repudiandae exercitationem doloremque. Beatae error facilis fugiat! Iste corrupti temporibus quia cupiditate quae vel quaerat non aut modi nisi ipsum voluptas delectus repudiandae, ex fuga, officiis cumque dolores iure sit?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor ullam provident a, quis eligendi quo quos cupiditate sequi inventore eos corrupti maiores explicabo accusantium, aliquam pariatur maxime unde, libero ipsa dignissimos quam? Numquam quibusdam non et praesentium, hic possimus quisquam velit consectetur vel at quasi cupiditate repellat ipsam id! Odit iure quo dolore natus recusandae soluta, autem, quidem quia quod aut suscipit accusantium iusto at qui quam architecto quisquam officiis repudiandae exercitationem doloremque. Beatae error facilis fugiat! Iste corrupti temporibus quia cupiditate quae vel quaerat non aut modi nisi ipsum voluptas delectus repudiandae, ex fuga, officiis cumque dolores iure sit?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
